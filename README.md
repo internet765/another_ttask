c5cd6bbc71ed3bda67c7147bdab4c244839537cc
Nikolay Gagara <ggn.f763@gmail.com>
2023-04-17 08:43:15 +0300

    верстка хедера

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 https://www.figma.com/file/rvSMYq3uZmcs5T3tVJZXzY/Untitled?node-id=4-309&t=MoMxMdFcDEPp3y6T-0
+
+
--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
 import './index.css';
 
-console.log('hello');
+// бургер меню
+
+window.addEventListener('resize', () => {
+  if (window.innerWidth >= 1024) {
+    document.querySelector('.header__menu').classList.remove('active');
+    document.querySelector('.burger').classList.remove('active');
+  }
+});
+
+document.querySelector('.burger').addEventListener('click', () => {
+  document.querySelector('.header__menu').classList.toggle('active');
+  document.querySelector('.burger').classList.toggle('active');
+});
